bce9672a32329480306868251b7ec26e1b105ae0
Jasmeet <jasmeetnrng@gmail.com>
2019-03-26 21:52:32 -0400

    added register compnent

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
 import React, { Component } from 'react';
 
+import {BrowserRouter, browserHistory, Route, Switch} from "react-router-dom";
+
+import Register from './Components/Register.js'
 
 class App extends Component {
   render() {
     return (
       <div>
         <h1>BAKERS BOOK</h1>
+          <BrowserRouter>
+              <div>
+                  <Route exact path = "/register" component={Register} />
+              </div>
+          </BrowserRouter>
       </div>
     );
   }
