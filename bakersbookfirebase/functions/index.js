96256de16bec7212e17966fd6b95fd568e3cc768
Matthew Li <matthewli2491@gmail.com>
2019-04-02 11:37:42 -0400

    created userinfo endpoint

--- bakersbookfirebase/functions/index.js
+++ bakersbookfirebase/functions/index.js
@@ -14,6 +14,7 @@
 const functions = require('firebase-functions');
 const express = require('express');
 var bodyParser = require('body-parser');
+var cors = require('cors')
 var admin = require('firebase-admin');
 const firebaseHelper = require('firebase-functions-helper');
 
@@ -22,9 +23,29 @@ admin.initializeApp(functions.config().firebase);
 const db = admin.firestore();
 
 const app = express();
+app.use(cors())
+
+// create application/json parser
+var jsonParser = bodyParser.json()
+// create application/x-www-form-urlencoded parser
+var urlencodedParser = bodyParser.urlencoded({ extended: false })
 
 app.get('/api/v1/', (req, res) => {
   res.send("This is BakersBook Api")
+});
+
+//Get a user's info with valid uid
+app.get('/api/v1/userinfo', jsonParser, (req, res) => {
+  admin.auth().getUser(req.body.uid)
+  .then((userRecord) => {
+    // See the UserRecord reference doc for the contents of userRecord.
+    console.log('Successfully fetched user data') 
+    res.json(userRecord);
+  })
+  .catch((error) => {
+    console.log('Error fetching user data:', error);
+  });
+
 });
 
 
