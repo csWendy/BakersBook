72ba49068c93d0d5c8bf2750dc3669cab5387083
Nirma Perera <nirmaperera23@gmail.com>
2019-05-06 20:29:34 -0400

    added author to view recipe

--- src/Components/Profile/UserRecipes.js
+++ src/Components/Profile/UserRecipes.js
@@ -56,22 +56,22 @@ class UserRecipes extends Component {
 		const { recipes } = this.state;
 		const recipeList = recipes.length ?
 			(recipes.map(recipe => {
-				return(
+				return (
 					<div className='recipe_Box' key={recipe.rid} >
 						<button className="deleteBtn" onClick={this.handleDelete(recipe.rid)} ><i className="fas fa-trash-alt"></i></button>
 						<NavLink to={"/viewRecipe/?ref=" + recipe.rid}>
-								<h2 className='recipe_title'><u>{recipe.name}</u></h2>
-								<img className="recipe__box-img" src={recipe.imageUrl} alt={recipe.name} />
-								<h4 className="recipe_ingredients">Ingredients: </h4>
-								{recipe.ingredient.map((aIngredient, index) => {
-									return (
-										<div key={index}>
-											<ul>
-												<li>{aIngredient.aIngredient}</li>
-											</ul>
-										</div>
-									)
-								})}
+							<h2 className='recipe_title'><u>{recipe.name}</u></h2>
+							<img className="recipe__box-img" src={recipe.imageUrl} alt={recipe.name} />
+							<h4 className="recipe_ingredients">Ingredients: </h4>
+							{recipe.ingredient.map((aIngredient, index) => {
+								return (
+									<div key={index}>
+										<ul>
+											<li>{aIngredient.aIngredient}</li>
+										</ul>
+									</div>
+								)
+							})}
 						</NavLink>
 					</div>
 				)
--- src/Components/ViewRecipe/ViewRecipe.js
+++ src/Components/ViewRecipe/ViewRecipe.js
@@ -1,74 +1,105 @@
 import React, { Component } from 'react';
 import axios from "axios";
 
-import bread from "../../assets/images/bread.jpg"
-
 import "./ViewRecipe.css"
-import { NavLink } from "react-router-dom";
+
 import queryString from "query-string";
 
 class ViewRecipe extends Component {
-    constructor(props) {
-        super(props)
-        this.state = {
-            // rid :rid,
-            name: "",
-            imageUrl: "",
-            category: "",
-            ingredient:[],
-            recipe: []
-        }
-    }
+	constructor(props) {
+		super(props)
+		this.state = {
+			author: "",
+			name: "",
+			imageUrl: "",
+			category: "",
+			ingredients: [],
+			recipe: []
+		}
+	}
 
-    componentDidMount() {
-        const rid = queryString.parse(this.props.location.search)
-        console.log("view Recipe", rid);
+	componentDidMount() {
+		window.scrollTo(0, 0);
+		const rid = queryString.parse(this.props.location.search)
+		console.log("view Recipe's rid", rid);
 
-        axios.get(`/api/v1/recipe/${rid.ref}`)
-            .then(response => {
-                this.setState({
-                    name: response.data.name,
-                    imageUrl: response.data.imageUrl,
-                    category: response.data.category,
-                    ingredient:response.data.ingredient
-                        .map(d=>d.aIngredient).join("\n"),
-                    recipe: response.data.recipe
-                        .map(d => d.instruction).join("\n")
-                })
-            })
-    }
 
-    render() {
-        return (
-            <div className="wrapper">
-                <div className="wrappingRecipe">
-                    <img className="recipeImage" src={this.state.imageUrl} />
-                    <form>
-                        <div>
-                            <input className="title" type="text" readOnly={true} value={this.state.name} />
-                        </div>
+		axios.get(`/api/v1/recipe/${rid.ref}`)
+			.then(response => {
+				console.log(response);
+				this.setState({
+					author: response.data.author,
+					name: response.data.name,
+					imageUrl: response.data.imageUrl,
+					category: response.data.category,
+					ingredients: response.data.ingredients,
+					recipe: response.data.recipe
+				}, () => {
+					console.log('The ingredients are: ', this.state.ingredients)
+				})
+			})
 
-                        <div className="recipeCategory">
-                            <label className="allLabel">Category:</label>
-                            <input className="categoryInput" readOnly={true} type="text" readOnly={true} value={this.state.category} />
-                        </div>
-                        <div className="recipeInstructions">
-                            <div>
-                                <label className="allLabel">Ingredients:</label>
-                            </div>
-                            <textarea className="instructionInput" readOnly={true} type="text" readOnly={true} value={this.state.ingredient} />
-                        </div>
-                        <div className="recipeInstructions">
-                            <div>
-                                <label className="allLabel">Instruction:</label>
-                            </div>
-                            <textarea className="instructionInput" readOnly={true} type="text" readOnly={true} value={this.state.recipe} />
-                        </div>
-                    </form>
-                </div>
-            </div>
-        );
-    }
+	}
+
+
+
+	render() {
+		return (
+			<div className="wrapper">
+				<button className="print-btn" onClick={() => window.print()}><i className="fas fa-print"></i> PRINT</button>
+				<div className="wrappingRecipe">
+					<img className="recipeImage" src={this.state.imageUrl} />
+					<form>
+						<div>
+							<input className="title" type="text" readOnly={true} value={this.state.name} />
+							<hr />
+							<div className="author-section">
+								<h4>By, <span>{this.state.author} </span></h4>
+							</div>
+						</div>
+
+						<div className="recipeCategory">
+							<label className="allLabel">Category:</label>
+							<input className="categoryInput" readOnly={true} type="text" readOnly={true} value={this.state.category} />
+						</div>
+						<div className="recipeInstructions">
+							<div>
+								<label className="allLabel">Ingredients:</label>
+							</div>
+
+							{this.state.ingredients.map((aIngredient, index) => {
+								return (
+									<div key={index}>
+										<ul>
+											<li>{aIngredient.aIngredient}</li>
+										</ul>
+									</div>
+								)
+							})}
+						</div>
+						<div className="recipeInstructions">
+							<div>
+								<label className="allLabel">Instruction:</label>
+							</div>
+							{this.state.recipe.map((instruction, i) => {
+								return (
+									<div key={i}>
+										<ul>
+											<label class="container">
+												<input type="checkbox" />
+												{instruction.instruction}
+												<span class="checkmark"></span>
+											</label>
+										</ul>
+									</div>
+								)
+							})}
+						</div>
+					</form>
+				</div>
+			</div>
+		);
+	}
 }
 
 export default ViewRecipe
